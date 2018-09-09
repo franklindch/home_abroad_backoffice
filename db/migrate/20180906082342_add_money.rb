d3f04bf494730e9eac7276b3b94577df5b443456
franklindch <f_dechanaud@hotmail.fr>
2018-09-09 16:34:51 +0200

    oli

--- db/migrate/20180906082342_add_money.rb
+++ db/migrate/20180906082342_add_money.rb
@@ -1,5 +1,5 @@
 class AddMoney < ActiveRecord::Migration[5.1]
   def change
-  	add_monetize :products, :application_fee_price_cents 
+  	add_monetize :invoices, :application_fee_price_cents 
   end
 end

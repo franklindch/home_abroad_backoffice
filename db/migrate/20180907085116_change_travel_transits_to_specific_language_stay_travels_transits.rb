8b25884a1f8a4a42afcf24c027fc9164236d640b
franklindch <f_dechanaud@hotmail.fr>
2018-09-09 16:42:18 +0200

    oli

--- db/migrate/20180907085116_change_travel_transits_to_specific_language_stay_travels_transits.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTravelTransitsToSpecificLanguageStayTravelsTransits < ActiveRecord::Migration[5.1]
-  def change
-  	rename_table :travel_transits, specific_language_stay_travels_transits
-  end
-end

9848e1d53d416f76821cd25f5dd65a1e4abad347
franklin de chanaud <f_dechanaud@hotmail.fr>
2018-10-17 09:41:17 +0200

    add city to families

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181002171212) do
+ActiveRecord::Schema.define(version: 20181017073950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -165,6 +165,7 @@ ActiveRecord::Schema.define(version: 20181002171212) do
     t.string "country"
     t.boolean "mother_main_contact", default: true
     t.boolean "father_main_contact", default: false
+    t.string "city"
     t.index ["qualification_id"], name: "index_families_on_qualification_id"
   end
 

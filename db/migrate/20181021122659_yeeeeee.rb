52407e13455d29e4d26e0c83dd98537589cddb83
franklin de chanaud <f_dechanaud@hotmail.fr>
2018-10-21 14:33:38 +0200

    column pasport_cni missing language_stays

--- db/migrate/20181021122659_yeeeeee.rb
+++ db/migrate/20181021122659_yeeeeee.rb
@@ -1,6 +1,5 @@
 class Yeeeeee < ActiveRecord::Migration[5.1]
   def change
-    remove_column :language_stays, :passport_cni, :boolean
     add_column :language_stays, :passport_cni, :boolean, default: false
   end
 end
